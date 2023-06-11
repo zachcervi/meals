cedd569203c7fa8f74da114a15af538a15fbbef5
Zach Cervi <zachcervi@gmail.com>
2023-06-11 17:34:11 -0600

    UI Updates
    Set Background colors
    Dynamically pass the category title

--- components/MealItem.js
+++ components/MealItem.js
@@ -1,11 +1,74 @@
-import { View, Text, StyleSheet } from "react-native";
+import {
+  Image,
+  View,
+  Text,
+  StyleSheet,
+  Pressable,
+  Platform,
+} from "react-native";
 
-function MealItem({ title }) {
+function MealItem({ title, imageUrl, duration, complexity, affordability }) {
   return (
-    <View>
-      <Text>{title}</Text>
+    <View style={styles.mealItem}>
+      <Pressable
+        android_ripple={{ color: "#ccc" }}
+        style={({ pressed }) => (pressed ? styles.buttonPressed : null)}
+      >
+        <View style={styles.innerContainer}>
+          <View>
+            <Image style={styles.image} source={{ uri: imageUrl }} />
+            <Text style={styles.title}>{title}</Text>
+          </View>
+          <View style={styles.details}>
+            <Text style={styles.detailItem}>{duration}m</Text>
+            <Text style={styles.detailItem}>{complexity.toUpperCase()}</Text>
+            <Text style={styles.detailItem}>{affordability.toUpperCase()}</Text>
+          </View>
+        </View>
+      </Pressable>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  mealItem: {
+    backgroundColor: "white",
+    borderRadius: 8,
+    elevation: 4,
+    margin: 16,
+    overflow: Platform.OS === "android" ? "hidden" : "visible",
+    shadowColor: "black",
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.25,
+    shadowRadius: 8,
+  },
+  innerContainer: {
+    borderRadius: 8,
+    overflow: "hidden",
+  },
+  image: {
+    height: 200,
+    width: "100%",
+  },
+  title: {
+    fontSize: 18,
+    fontWeight: "bold",
+    margin: 8,
+    textAlign: "center",
+  },
+  details: {
+    alignItems: "center",
+    flexDirection: "row",
+    justifyContent: "center",
+    padding: 8,
+  },
+  detailItem: {
+    fontSize: 12,
+    marginHorizontal: 4,
+  },
+  buttonPressed: {
+    opacity: 0.5,
+  },
+});
 
 export default MealItem;
